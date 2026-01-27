41611cef945d7debafa53985852555d7721b0dcd
Deepanshu Bhagria <deepanshubhagria@mac.mynetworksettings.com>
2026-01-26 22:52:30 -0500

    Fix Vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<my-portfolio>/'
+  // Configure base to serve correctly from GitHub Pages repo path
+  base: '/my-portfolio/'
 
 })
