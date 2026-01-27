cdd9a7dfb850ce2c7fe1f610afee61f685f79f33
Deepanshu Bhagria <deepanshubhagria@mac.mynetworksettings.com>
2026-01-26 22:58:22 -0500

    Fix Vite base path

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Projects from "./components/Projects.jsx";
+import Contact from "./components/Contact.jsx";
+import Experience from "./components/Experience.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+import "./styles/layout.css";
+import "./styles/hero.css";
+import "./styles/about.css";
+import "./styles/projects.css";
+
+function App() {
+  return (
+    <div className="layout">
+      <aside className="layout__left">
+        <div className="layout__leftInner">
+          <Hero />
+        </div>
+      </aside>
+
+      <main className="layout__right">
+        <div className="layout__rightInner">
+          <About />
+          <Experience />
+          <Projects />
+          <footer className="footer">Thankyou for visiting my website!</footer>
+        </div>
+      </main>
+    </div>
+  );
+}
+createRoot(document.getElementById("root")).render(<App />);
